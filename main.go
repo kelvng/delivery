6a1ea4b864283b976fc3a9413a98285370e2cd34
Thanh Khang <nthanhkhang@outlook.com>
2022-10-29 13:00:25 +0700

    create data

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	"os"
 )
 
+type Restaurant struct {
+	Id   int    `json:"id" gorm:"column:id;"` //tag
+	Name string `json:"name" gorm:"column:name;"`
+	Addr string `json:"addr" gorm:"column:addr;"`
+}
+
+func (Restaurant) TableName() string {
+	return "restaurants"
+}
+
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	//dsn := "root:@tcp(127.0.0.1:3306)/delivery?charset=utf8mb4&parseTime=True&loc=Local"
@@ -18,5 +28,12 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	log.Println(db, err)
+	newRestaurant := Restaurant{Name: "Khang", Addr: "Ben Tre"}
+
+	if err := db.Create(&newRestaurant).Error; err != nil {
+		log.Println(err)
+	} else {
+		log.Println("New id:", newRestaurant.Id)
+	}
+
 }
