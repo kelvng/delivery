445c854bb10b03e5748192da03393b6e1063fb37
Thanh Khang <khangnt@proton.me>
2022-12-24 14:05:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -35,14 +35,21 @@ func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	//dsn := "root:@tcp(127.0.0.1:3306)/delivery?charset=utf8mb4&parseTime=True&loc=Local"
 
-	dsn := os.Getenv("MYSQL_CONN_STRING") // env
+	dsn := os.Getenv("bc8c4cf9cd5307:de804454@us-cdbr-east-06.cleardb.net/heroku_1e50603699e4adf?reconnect=true") // env
 
-	s3BucketName := os.Getenv("S3BucketName")
-	s3Region := os.Getenv("S3Region")
-	s3APIKey := os.Getenv("S3APIKey")
-	s3SecretKey := os.Getenv("S3SecretKey")
-	s3Domain := os.Getenv("S3Domain")
-	secretKey := os.Getenv("SYSTEM_SECRET")
+	//s3BucketName := os.Getenv("S3BucketName")
+	//s3Region := os.Getenv("S3Region")
+	//s3APIKey := os.Getenv("S3APIKey")
+	//s3SecretKey := os.Getenv("S3SecretKey")
+	//s3Domain := os.Getenv("S3Domain")
+	//secretKey := os.Getenv("SYSTEM_SECRET")
+
+	s3BucketName := os.Getenv("delivery-golang")
+	s3Region := os.Getenv("ap-southeast-1")
+	s3APIKey := os.Getenv("AKIA2LX3UTKASMGBRKNV")
+	s3SecretKey := os.Getenv("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")
+	s3Domain := os.Getenv("https://d2ygp6qy0o4yxw.cloudfront.net")
+	secretKey := os.Getenv("MaiKhaAi")
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -72,5 +79,5 @@ func main() {
 	setupRoute(appContext, v1)
 	setupAdminRoute(appContext, v1)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
